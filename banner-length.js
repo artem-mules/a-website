e3cd4159eadd907ec5ad52cce9985664fb85aca0
Artem Kopytok <13ar.bass@gmail.com>
2022-02-17 19:27:58 +0300

    updated

---
+++ banner-length.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", function () {
+    let bannerDescription = document.querySelector('p.banner-description.banner-description--banner');
+    bannerDescription.textContent = bannerDescription.textContent.slice(0, 63).concat('...');
+});

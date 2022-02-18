c50c4870a57f30443ef9bb41320341fabc927e33
Artem Kopytok <13ar.bass@gmail.com>
2022-02-18 10:03:18 +0300

    updated

--- become-a-member.js
+++ become-a-member.js
@@ -1,6 +1,9 @@
-let allCheckboxItems = document.querySelectorAll('.cl-i__merch');
+window.addEventListener("load", function () {
+    let allCheckboxItems = document.querySelectorAll('.cl-i__merch');
+    document.querySelector('.cl-i__merch--checkbox').click();
 
-allCheckboxItems.forEach(item => {
-    currentInputName = item.querySelector('[to-copy]').getAttribute('to-copy');
-    item.querySelector('input').setAttribute('name', currentInputName);
+    allCheckboxItems.forEach(item => {
+        currentInputName = item.querySelector('[to-copy]').getAttribute('to-copy');
+        item.querySelector('input').setAttribute('name', currentInputName);
+    });
 });
